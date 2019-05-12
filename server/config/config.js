c808ec589cafc6a57de2e717f6c1a4c4ae013053
Qocotzxin <cristianmetchebarne@gmail.com>
2019-05-12 15:58:36 -0300

    Env variable added for Mongo Connection

--- server/config/config.js
+++ server/config/config.js
@@ -3,8 +3,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 const localDB = "mongodb://localhost:27017/coffe";
-const productionDB =
-  "mongodb+srv://cetchebarne:W6ItE4oyMPen8igE@api-rest-node-u4dlb.mongodb.net/coffe?retryWrites=true";
+const productionDB = process.env.MONGO_URI;
 
 let urlDB = process.env.NODE_ENV === "dev" ? localDB : productionDB;
 
